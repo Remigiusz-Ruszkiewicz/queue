e7b02034fb6ccfacf923d14c63a7ac8fd31ac470
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-19 18:45:22 +0200

    Add project files.

---
+++ queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack collection = new Stack();
+            collection.Push("Anna");
+            collection.Push(12);
+            collection.Push(true);
+            while (collection.Count != 0)
+            {
+                Console.WriteLine(collection.Pop());
+            }
+            
+            Console.Read();
+        }
+    }
+}
